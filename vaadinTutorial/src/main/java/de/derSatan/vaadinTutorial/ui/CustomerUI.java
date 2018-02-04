c04bef0274f057445d788a9668d4a33a5527c9ad
jenshardt@gmx.net <derSatan>
2018-02-04 19:47:09 +0100

    new tutorial

--- vaadinTutorial/src/main/java/de/derSatan/vaadinTutorial/ui/CustomerUI.java
+++ vaadinTutorial/src/main/java/de/derSatan/vaadinTutorial/ui/CustomerUI.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -74,6 +79,34 @@ public class CustomerUI extends UI {
 
 		// Initialize listing
 		listCustomers(null);
+		
+		// Create the accordion
+		Accordion accordion = new Accordion();
+
+		// Create the first tab, specify caption when adding
+		Layout tab1 = new VerticalLayout(); // Wrap in a layout
+		tab1.addComponent(new Image(null, // No component caption
+		    new ThemeResource("img/planets/Mercury.jpg")));
+		accordion.addTab(tab1, "Mercury",
+		    new ThemeResource("img/planets/Mercury_symbol.png"));
+
+		// This tab gets its caption from the component caption
+		Component tab2 = new Image("Venus",
+		    new ThemeResource("img/planets/Venus.jpg"));
+		accordion.addTab(tab2).setIcon(
+		    new ThemeResource("img/planets/Venus_symbol.png"));
+
+		// And so forth the other tabs...
+		String[] tabs = {"Earth", "Mars", "Jupiter", "Saturn"};
+		for (String caption: tabs) {
+		    String basename = "img/planets/" + caption;
+		    VerticalLayout tab = new VerticalLayout();
+		    tab.addComponent(new Image(null,
+		            new ThemeResource(basename + ".jpg")));
+		    accordion.addTab(tab, caption,
+		            new ThemeResource(basename + "_symbol.png"));
+		}
+		mainLayout.addComponent(accordion);
 	}
 
 	void listCustomers(String filterText) {
